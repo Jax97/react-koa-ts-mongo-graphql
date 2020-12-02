4dd6a397b3ce4790385ed168e19c050aa961595d
Jax97 <1319334666@qq.com>
2020-12-02 15:28:45 +0800

    数据库连接兼容错误

--- src/server/utils/DBHelper.ts
+++ src/server/utils/DBHelper.ts
@@ -1,9 +1,26 @@
 import mongoose from 'mongoose';
 import config from '../config/index';
 
+const options = {
+    // reconnectInterval: 1000,
+    // reconnectTries: 30,
+    // poolSize: 10,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+}
+
 const DBHelper = {
     connect(): mongoose.Mongoose {
-        mongoose.connect(config.databaseUrl)
+        mongoose.connect(config.databaseUrl, options);
+        const db = mongoose.connection;
+
+        db.once('error', () => {
+            console.error('数据库连接失败');
+        })
         return mongoose;
     }
 }
+
+export default DBHelper;
